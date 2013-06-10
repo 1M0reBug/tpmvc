df6f027e5cea0c4ba97b4ab3b12bb6b3d914ebd3
cexbrayat <cexbrayat@gmail.com>
2013-06-10 23:29:32 +0200

    bootstrap & jquery to do ajax stuff

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,3 +1,44 @@
-$(function(){
-    $("#who").html("a dynamic content");
-})
+var showSpectacles = function (spectacles) {
+    var html = "<table class='table table-bordered'><tr>" +
+        "<th>Artist</th>" +
+        "<th>Title</th>" +
+        "<th></th>" +
+        "</tr>";
+    spectacles.forEach(function (spectacle) {
+        html += "<tr>" +
+            "<td>" + spectacle.artiste + "</td>" +
+            "<td>" + spectacle.titre + "</td>" +
+            "<td><a class='btn' onclick='detail(" + spectacle.id + ")'><i class='icon-search'></i></a></td>" +
+            "</tr>"
+    });
+    html += "</table>"
+    $("#spectacles").html(html);
+};
+
+var showSpectacle = function (spectacle) {
+    var html = "<h3>" + spectacle.artiste + " - " + spectacle.titre + "</h3>" +
+        "<div>" + spectacle.type + "</div>" +
+        "<div><a onclick='list()'>Liste</a></div>";
+    $("#spectacles").html(html);
+};
+
+var list = function () {
+    $.getJSON("http://localhost:8080/tpmvc/api/spectacles", function (data) {
+        showSpectacles(data);
+    });
+    /*
+     showSpectacles([
+     {"id": 1000, "titre": "Iron", "artiste": "Woodkid", "type": "CONCERT", "version": 0},
+     {"id": 1001, "titre": "Get Lucky", "artiste": "Daft Punk", "type": "CONCERT", "version": 0}
+     ]);
+     */
+};
+
+var detail = function (id) {
+    $.getJSON("http://localhost:8080/tpmvc/api/spectacles/" + id, function (data) {
+        showSpectacle(data);
+    });
+    //showSpectacle({"id": 1000, "titre": "Iron", "artiste": "Woodkid", "type": "CONCERT", "version": 0});
+};
+
+list();
